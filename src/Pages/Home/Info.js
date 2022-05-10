e344008dd4d7a813e9f81b627d26d94df09cc7bc
thowhidulislam <thowhid.ndc14@gmail.com>
2022-05-10 22:45:01 +0600

    footer added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -10,7 +10,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             <InfoCard cardTitle={'Opening Hours'} bgColor='bg-gradient-to-r from-secondary to-primary' img={clock}></InfoCard>
-            <InfoCard cardTitle='Visit our location' bgColor='bg-accent' img={marker}></InfoCard>
+            <InfoCard cardTitle='Visit our location' bgColor='bg-neutral' img={marker}></InfoCard>
             <InfoCard cardTitle='Contact us now' bgColor='bg-gradient-to-r from-secondary to-primary' img={phone}></InfoCard>
         </div>
     );
